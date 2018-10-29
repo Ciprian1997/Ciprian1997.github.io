e559232e0465dbfc171171265e569febc944a56a
Ciprian1997 <43964545+Ciprian1997@users.noreply.github.com>
2018-10-29 20:26:55 +0200

    string

--- js/learn.js
+++ js/learn.js
@@ -1,1 +1,18 @@
 console.info(0);
+
+var prenume = "Ciprian";
+var nume = "Gros";
+var owner = "Ciprian's";
+var msg1= 'ai zis "Nick"';
+var msg2= 'Nu, am zis Ciprian'
+var template= `'Text cu ghilimele " sau simple'`;
+
+console.debug("auzi....? dar cum te cheama? -" + prenume);
+
+var skills = ['html', 'css', 'js'];
+
+prenume = "Nick";
+
+console.info("toate variabilele au fost initializate");
+console.log(nume);
+console.debug("cum te cheama? -" + prenume);
