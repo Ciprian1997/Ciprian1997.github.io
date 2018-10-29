86a173004d8f8c8d5dd9f1b9471d1ab5101c1d7a
Ciprian1997 <43964545+Ciprian1997@users.noreply.github.com>
2018-10-29 21:42:37 +0200

    comision aplicat

--- js/learn.js
+++ js/learn.js
@@ -20,19 +20,33 @@ console.info("toate variabilele au fost initializate");
 console.log(nume);
 console.debug("cum te cheama? -" + prenume);
 sayWelcome();
-sayWelcome();
 
 // schimb valutar
 
 var cursEUR = 4.5;
 var myEUR = 20;
 var myRON = 0;
-var friendEUR = 10;
+var friendEUR = 100;
 var friendRON = 0;
 // operatori * / + -
 
 myRON = myEUR * cursEUR;
 console.info("eu am " + myRON + "RON");
 
-friendRON = friendEUR * cursEUR;
+friendRON = friendEUR * cursEUR - (friendEUR * cursEUR * 1) / 100;
 console.info("tu ai " + friendRON + "RON");
+
+function extractFromATM(ammount) {
+  console.info("==== ==== ==== ==== ==== ");
+  console.info("suma extrasa este: " + ammount);
+  var comision = ammount * 0.01;
+  if (comision < 2.5) {
+    comision = 2.5;
+    console.warn("comision minim aplicat");
+  }
+  console.info("comison aplicat: " + comision);
+  console.info("==== ==== ==== ==== ==== ");
+}
+
+extractFromATM(1005);
+extractFromATM(100);
