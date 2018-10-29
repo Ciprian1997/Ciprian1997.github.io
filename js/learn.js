101e7cf2b15d726bde75d86b11cd14054217ea83
Ciprian1997 <43964545+Ciprian1997@users.noreply.github.com>
2018-10-29 20:58:58 +0200

    add simple exchange rage

--- js/learn.js
+++ js/learn.js
@@ -1,18 +1,38 @@
 console.info(0);
 
+function sayWelcome() {
+  console.info("Salut " + prenume + ", bine ai venit la noi!");
+}
 var prenume = "Ciprian";
 var nume = "Gros";
 var owner = "Ciprian's";
-var msg1= 'ai zis "Nick"';
-var msg2= 'Nu, am zis Ciprian'
-var template= `'Text cu ghilimele " sau simple'`;
+var msg1 = 'ai zis "Cipsa"';
+var msg2 = "Nu, am zis Ciprian";
+var template = `'Text cu ghilimele " sau simple'`;
 
 console.debug("auzi....? dar cum te cheama? -" + prenume);
+sayWelcome();
+var skills = ["html", "css", "js"];
 
-var skills = ['html', 'css', 'js'];
-
-prenume = "Nick";
+prenume = "Cipsa";
 
 console.info("toate variabilele au fost initializate");
 console.log(nume);
 console.debug("cum te cheama? -" + prenume);
+sayWelcome();
+sayWelcome();
+
+// schimb valutar
+
+var cursEUR = 4.5;
+var myEUR = 20;
+var myRON = 0;
+var friendEUR = 10;
+var friendRON = 0;
+// operatori * / + -
+
+myRON = myEUR * 4.7;
+console.info("eu am " + myRON + "RON");
+
+friendRON = friendEUR * 4.7;
+console.info("tu ai " + friendRON + "RON");
