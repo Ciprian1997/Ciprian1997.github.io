77d100e8141991dba39cc745d80f061bcdd9b941
Ciprian1997 <leul_focului@yahoo.com>
2018-11-12 21:52:44 +0200

    show pages based on selected menu

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-document.getElementById("app").innerHTML = `
-<h1>Hello Parcel!</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
-`;
+function showHomePage(){
+  document.getElementById("home-page").style.display = "block";
+  document.getElementById("languages-page").style.display = "none";
+  document.getElementById("skills-page").style.display = "none";
+}
+function showSkillsPage(){
+  document.getElementById("home-page").style.display = "none";
+  document.getElementById("languages-page").style.display = "none";
+  document.getElementById("skills-page").style.display = "block";
+}
+function showLanguagesPage(){
+  document.getElementById("home-page").style.display = "none";
+  document.getElementById("languages-page").style.display = "block";
+  document.getElementById("skills-page").style.display = "none";
+}
+document.getElementById("home-menu").onclick = showHomePage;
+document.getElementById("languages-menu").onclick = showLanguagesPage;
+document.getElementById("skills-menu").onclick = showSkillsPage;
