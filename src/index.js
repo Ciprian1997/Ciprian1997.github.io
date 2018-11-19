39456fb09e48976a01387278af6c030b734a1492
Ciprian1997 <leul_focului@yahoo.com>
2018-11-19 19:53:46 +0200

    add education page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 function $(id){
   return document.getElementById(id);
-
 }
 function hide(id) {
   $(id).style.display = "none";
@@ -13,17 +12,29 @@ function showHomePage(){
   show("home-page");
   hide("languages-page");
   hide("skills-page");
+  hide("education-page");
 }
 function showSkillsPage(){
   hide("home-page");
   hide("languages-page");
   show("skills-page");
+  hide("education-page");
+}
+function showEducationPage(){
+  hide("home-page");
+  hide("languages-page");
+  show("education-page");
+  hide("skills-page");
 }
 function showLanguagesPage(){
   hide("home-page");
   show("languages-page");
   hide("skills-page");
+  hide("education-page");
 }
+
+
 $("home-menu").onclick = showHomePage;
 $("languages-menu").onclick = showLanguagesPage;
 $("skills-menu").onclick = showSkillsPage;
+$("education-menu").onclick = showEducationPage;
