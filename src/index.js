87bdb74fb9bbcac3acb30c85a241009fd6a268f2
Ciprian1997 <leul_focului@yahoo.com>
2018-12-02 18:56:48 +0200

    skills js

--- src/index.js
+++ src/index.js
@@ -13,17 +13,34 @@ function hideElement(el) {
 }
 
 function hideAllPages() {
-  var pages = document.querySelectorAll(" .page-block");
+  var pages = document.querySelectorAll(".page-block");
   pages.forEach(hideElement);
 }
 
-var links = document.querySelectorAll("top-menu-bar a");
-for(var i = 0; i < links.length; i++) {
-  links[i].onclick = function(){
-    hideAllPages();
-    var page = this.getAttribute('data-page');
-    show(page + "-page");
-  };
+function initMenu() {
+  var links = document.querySelectorAll("#top-menu-bar a");
+  for(var i = 0; i < links.length; i++) {
+    links[i].onclick = function(){
+      hideAllPages();
+      var page = this.getAttribute('data-page');
+      show(page + "-page");
+    };
+  }
 }
 
+function initSkillsPage() {
+  var skills = ['js' , 'HTML', 'css'];
+  var resultList = document.querySelector('#skills-page ul');
 
+  var listItems = skills.map(function(skill) {
+    return `<li>${skill.toUpperCase()}</li>`;
+  })
+
+  console.log('resultList:', listItems);
+  resultList.innerHTML = listItems.join('');
+} 
+
+
+initMenu();
+show('skills-page');
+initSkillsPage();
